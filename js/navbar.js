6f0b30a3003e05c0fdcfe3f2ad2e1411cb74ad58
Leo <leonard.lemke@hs-furtwangen.de>
2022-07-28 01:03:24 +0200

    optimizations

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,4 @@
+/*  Navbar  */
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
@@ -5,3 +6,17 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+
+/*  Language  */
+if (isGerman()) {
+  $('[lang="en"]').hide();
+} else {
+  $('[lang="de"]').hide();
+}
+
+function isGerman() {
+  let userLang = navigator.language || navigator.userLanguage;
+  return userLang.toLowerCase().includes('de');
+}
+//not working: https://stackoverflow.com/a/43033380/18332741
